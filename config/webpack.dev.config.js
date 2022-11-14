98fe36a317dd46116a65183d833748bbd29c9e2b
Thomi At Gojek <thomi.jasir@gojek.com>
2022-11-14 09:42:49 +0700

    chore(init): initial commit

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,89 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const paths = require('../scripts/paths');
+const packageJson = require('../package.json');
+
+const { ModuleFederationPlugin } = webpack.container;
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [paths.appIndexJs],
+  mode: 'development',
+  output: {
+    publicPath: '/',
+    path: paths.appBuild,
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[name].[hash][ext][query]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        include: [paths.appSrc],
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: paths.appPostCssConfig,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ico|gif|jpe?g|jpg|png|svg|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      Components: paths.resolveApp('src/components'),
+      Containers: paths.resolveApp('src/container'),
+      Utils: paths.resolveApp('src/utils'),
+    },
+  },
+  plugins: [
+    new Dotenv(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebPackPlugin({
+      template: paths.appHtml,
+      favicon: paths.appFavicon,
+      inject: true,
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    // https://github.com/ogzhanolguncu/react-typescript-module-federation
+    // https://webpack.js.org/concepts/module-federation/
+    new ModuleFederationPlugin({
+      name: 'mfesbootstrap',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './GoogleBtn': './src/components/GoogleBtn/GoogleBtn.comp.tsx',
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
---
+++ src/bootstrap.tsx
@@ -0,0 +1,47 @@
+// import 'react-app-polyfill/stable';
+import React, { FC, useContext, useMemo, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Router from './services/Router';
+import ClearCache from './ClearCache';
+import AppProvider, { AppContext } from './store/AppProvider';
+import LoadingGeneral from './components/LoadingGeneral/LoadingGeneral.comp';
+import ErrorGeneral from './components/ErrorGeneral/ErrorGeneral.comp';
+import './assets/app.css';
+
+const queryClient = new QueryClient();
+
+const App: FC = () => {
+  // ? SET FALSE IF YOU REQUIRED FETCH DATA BEFORE RENDER ROUTER
+  const [appReady] = useState<boolean>(true);
+  const context = useContext(AppContext);
+
+  // useEffect(() => {
+  // * IT WILL USE IF APP REQUIRED FETCH DATA BEFORE RENDER CONTAINER
+  // ? FOR SOME CASE THAT APP NEED TOKEN BEFORE RENDER
+  // ! setAppReady(true);
+  // }, []);
+
+  // * MEMOIZE ROUTER CAN HELP INCREASE INDEX PERFORMANCE APP
+  const renderRouter = useMemo(() => appReady && <Router />, [appReady]);
+
+  return (
+    <>
+      <LoadingGeneral {...context.loadingState} />
+      <ErrorGeneral {...context.errorState} />
+      {renderRouter}
+    </>
+  );
+};
+
+ReactDOM.createRoot(
+  document.getElementById('root') || document.createDocumentFragment(),
+).render(
+  <ClearCache>
+    <QueryClientProvider client={queryClient}>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </QueryClientProvider>
+  </ClearCache>,
+);
